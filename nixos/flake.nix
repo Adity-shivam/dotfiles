20aeb5b7a7f0a9073a853f37cbf5a3ecf37614e5
Adity-shivam <adishivam1507@gmail.com>
2026-09-07 17:14:35 +0530

    removed nixvim nixpkgs follows

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -17,7 +17,7 @@
 
     nixvim = {
       url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
     # import inputs.nixvim.homeModules.nixvim 
     # import inputs.nixvim.nixosModules.nixvim  

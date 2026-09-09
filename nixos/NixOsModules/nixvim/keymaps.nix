5d72be3bab4b546c9231e31979f80c9c8f86035c
Adity-shivam <adishivam1507@gmail.com>
2026-09-09 14:19:12 +0530

    renamed keybinds to keymaps

--- nixos/NixOsModules/nixvim/keymaps.nix
+++ nixos/NixOsModules/nixvim/keymaps.nix
@@ -6,10 +6,21 @@
 
   programs.nixvim.keymaps = [
     {
+      # mode = "nv";
       action = "<cmd>Neotree<CR>";
       key = "\\";
       options = {
         silent = true;
+      };
+    }
+
+    {
+      mode = "n";
+      key = <Esc>;
+      action = "<cmd>nohlsearch<CR>";
+      options = {
+        silent = true;
+        # noremap = true; # defaults to true i.e no recursive binds
       };
     }
 
--- nixos/NixOsModules/nixvim/nixvim.nix
+++ nixos/NixOsModules/nixvim/nixvim.nix
@@ -5,7 +5,7 @@
   imports = [ 
     ./opts.nix
     ./plugins.nix
-    ./keybinds.nix
+    ./keymaps.nix
   ];
 	
   # Enable NixVim

c731590378f6b1123c4599ab279a8c5c2ba29841
Adity-shivam <adishivam1507@gmail.com>
2026-09-11 00:31:04 +0530

    trying nixvim

--- nixos/NixOsModules/nixvim/lsp.nix
+++ nixos/NixOsModules/nixvim/lsp.nix
@@ -25,6 +25,28 @@ in
 
 {
   lsp = {
-    enable = true;
+    servers = [
+      clangd = {
+        config = {
+          cmd = [
+            "clangd"
+            "--background-index"
+          ];
+          filetypes = [
+            "c"
+            "cpp"
+          ];
+          root_markers = [
+            "compile_commands.json"
+            "compile_flags.txt"
+          ];
+        };
+        enable = true;
+      };
+    
+
+    ];
+  
+
   };
 }
